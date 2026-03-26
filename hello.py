d591086afc81f6dba38bc238246f7292c177c107
NijayaSenarath <137043063+nijsen@users.noreply.github.com>
2026-03-26 20:46:24 +1300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Simple Hello World program
+
+def greet(name):
+    print(f"Hello, {name}! Welcome to NijayaTest repository.")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet(user_name)
